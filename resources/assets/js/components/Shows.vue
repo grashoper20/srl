19b060cad86478a52edf07ba2f3b717bd21e1317
Grashoper <grashoper20@yahoo.ca>
2017-09-23 13:41:28 -0500

    Basic search functionality.

--- resources/assets/js/components/Shows.vue
+++ resources/assets/js/components/Shows.vue
@@ -1,15 +1,16 @@
 <template>
     <div id="content" class="container">
+        <input type="text" v-model="search">
         <div class="shows">
             <h2>Shows</h2>
             <div class="shows--list">
-                <show v-for="show in shows" v-bind:show="show"></show>
+                <show v-for="show in shows" :key="show.show_id" v-bind:show="show"></show>
             </div>
         </div>
         <div class="anime" v-if="anime && anime.length">
             <h2>Anime</h2>
             <div class="shows--list">
-                <show v-for="show in anime" v-bind:show="show"></show>
+                <show v-for="show in anime" :key="show.show_id" v-bind:show="show"></show>
             </div>
         </div>
 
@@ -23,29 +24,71 @@
 
 <script>
     import axios from 'axios';
+    import Fuse from 'fuse.js';
 
     export default {
         data: () => ({
+            errors: [],
+            full_list: [],
+            filtered_list: [],
             shows: [],
             anime: [],
-            errors: []
+            search: '',
+            fuse: {},
         }),
         created() {
+            this.fuse_options = {
+                shouldSort: true,
+                threshold: 0.2,
+                location: 0,
+                distance: 100,
+                maxPatternLength: 32,
+                minMatchCharLength: 1,
+                keys: [
+                    'show_name',
+                ]
+            };
             axios.get('/api/show')
                 .then(response => {
-                    let self = this;
-                    response.data.forEach(function (fetched_show) {
-                        if (parseInt(fetched_show.anime)) {
-                            self.anime.push(fetched_show);
-                        }
-                        else {
-                            self.shows.push(fetched_show);
-                        }
-                    });
+                    this.full_list = response.data;
                 })
                 .catch(e => {
                     this.errors.push(e)
                 })
+        },
+        watch: {
+            search() {
+                console.log('triggering search');
+                this.triggerSearch();
+            },
+            full_list() {
+                this.fuse = new Fuse(this.full_list, this.fuse_options);
+                this.triggerSearch();
+            },
+            filtered_list() {
+                let anime = [], shows = [];
+                this.filtered_list.forEach(function (show) {
+                    if (parseInt(show.anime)) {
+                        anime.push(show);
+                    }
+                    else {
+                        shows.push(show);
+                    }
+                });
+                this.anime = anime;
+                this.shows = shows;
+            },
+        },
+        methods: {
+            triggerSearch: function() {
+                console.log("search triggered:" + this.search);
+                let keyworks = this.search.trim();
+                if (keyworks === '') {
+                    this.filtered_list = this.full_list;
+                } else {
+                    this.filtered_list = this.fuse.search(keyworks);
+                }
+            }
         }
     }
 </script>
@@ -56,5 +99,6 @@
         flex-flow: row wrap;
         justify-content: center;
     }
+
     //
 </style>
