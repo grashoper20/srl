6479bef5291791a173e456c5bfc8965421573ebd
Grashoper <grashoper20@yahoo.ca>
2017-10-06 11:06:08 -0500

    Status processing in mixin.

--- resources/assets/js/components/ShowEpisodeList.vue
+++ resources/assets/js/components/ShowEpisodeList.vue
@@ -15,13 +15,13 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr v-bind:class="episodeClass(episode.status)" v-for="episode in episodes.slice().reverse()">
+                <tr v-bind:class="statusClass(episode.status)" v-for="episode in episodes.slice().reverse()">
                     <td class="episode--nfo">{{episode.hasnfo ? 'Y' : 'N'}}</td>
                     <td class="episode--tbn">{{episode.hastbn ? 'Y' : 'N'}}</td>
                     <td class="episode--episode">{{episode.episode}}</td>
                     <td class="episode--name w-100">{{episode.name}}</td>
                     <td class="episode--airdate">{{formatAirDate(episode.airdate)}}</td>
-                    <td class="episode--status">{{episodeStatusText(episode.status)}}</td>
+                    <td class="episode--status">{{statusText(episode.status)}}</td>
                 </tr>
                 </tbody>
             </table>
@@ -30,6 +30,7 @@
 </template>
 
 <script>
+    import StatusMixin from '../mixins/Status';
     import moment from 'moment';
 
     export default {
@@ -37,66 +38,13 @@
             'seasons'
         ],
         computed: {},
+        mixins: [StatusMixin,],
         methods: {
             formatAirDate(airdate) {
                 if (airdate === '1') {
                     return 'Never';
                 }
                 return moment(airdate).format('YYYY-MM-DD');
-            },
-            episodeClass(state) {
-                if (state > 100) {
-                    state %= 100;
-                }
-                switch (state) {
-                    case 1:
-                        return 'unaired';
-                    case 2:
-                    case 9:
-                    case 12:
-                        return 'snatched';
-                    case 3:
-                        return 'wanted';
-                    case 4:
-                        return 'good';
-                    case 5:
-                    case 7:
-                        return 'skipped';
-                    case 6:
-                        return 'archived';
-                    case 50:
-                        return 'qual';
-                }
-                console.warn('Unhandled state class: ' + state);
-                return 'weird-' + state;
-            },
-            episodeStatusText(state) {
-                if (state > 100) {
-                    state %= 100;
-                }
-                switch (state) {
-                    case 1:
-                        return 'unaired';
-                    case 2:
-                    case 9:
-                    case 12:
-                        return 'snatched';
-                    case 3:
-                        return 'wanted';
-                    case 4:
-                        return 'good';
-                    case 5:
-                        return 'skipped';
-                    case 6:
-                        return 'archived';
-                    case 7:
-                        return 'ignored';
-                    case 50:
-                        return 'qual';
-                    default:
-                        console.warn('Unhandled state text: ' + state);
-                        return 'weird-' + state;
-                }
             },
             seasonName(season) {
                 return season[0].season || 'Specials';
@@ -142,24 +90,6 @@
         }
         .episode--name {
             text-align: left;
-        }
-        .unaired {
-            background-color: #F5F1E4;
-        }
-        .good {
-            background-color: #C3E3C8;
-        }
-        .qual {
-            background-color: #FFDA8A;
-        }
-        .skipped {
-            background-color: #BEDEED;
-        }
-        .wanted {
-            background-color: #FFB0B0;
-        }
-        .snatched {
-            background-color: #EBC1EA;
         }
     }
 </style>
---
+++ resources/assets/js/mixins/Status.js
@@ -0,0 +1,57 @@
+function ensureState(state) {
+    return state > 100 ? (state % 100) : state;
+}
+
+export default {
+    methods: {
+        statusClass(state) {
+            state = ensureState(state);
+            switch (state) {
+                case 1:
+                    return 'status-unaired';
+                case 2:
+                case 9:
+                case 12:
+                    return 'status-snatched';
+                case 3:
+                    return 'status-wanted';
+                case 4:
+                    return 'status-good';
+                case 5:
+                case 7:
+                    return 'status-skipped';
+                case 6:
+                    return 'status-archived';
+                case 50:
+                    return 'status-qual';
+            }
+            console.warn('Unhandled state class: ' + state);
+            return 'status-weird-' + state;
+        },
+        statusText(state) {
+            state = ensureState(state);
+            switch (state) {
+                case 1:
+                    return 'unaired';
+                case 2:
+                case 9:
+                case 12:
+                    return 'snatched';
+                case 3:
+                    return 'wanted';
+                case 4:
+                    return 'good';
+                case 5:
+                    return 'skipped';
+                case 6:
+                    return 'archived';
+                case 7:
+                    return 'ignored';
+                case 50:
+                    return 'qual';
+            }
+            console.warn('Unhandled state text: ' + state);
+            return 'weird-' + state;
+        },
+    }
+}
