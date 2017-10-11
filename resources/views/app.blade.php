611d978fb1cc48f152665b5396f18df695cd1c0f
Grashoper <grashoper20@yahoo.ca>
2017-10-11 07:45:22 -0500

    Split out vendor js into its own file
    This might help with delivery on non http2 connections but it will help
    more with tracking where application growth is happening.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,6 +23,8 @@
                 <router-view></router-view>
             </section>
         </div>
+        <script src="{{ url(mix('js/manifest.js')) }}"></script>
+        <script src="{{ url(mix('js/vendor.js')) }}"></script>
         <script src="{{ url(mix('js/app.js')) }}"></script>
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -25,4 +25,5 @@ mix.webpackConfig({
     ]
 })
     .js('resources/assets/js/app.js', 'public/js')
+        .extract(['vue', 'vuex', 'vue-js-modal', 'vue-localstorage', 'vue-router', 'lodash', 'jquery', 'popper.js', 'bootstrap', 'axios', 'moment', 'fuse.js'])
     .sass('resources/assets/sass/app.scss', 'public/css');
