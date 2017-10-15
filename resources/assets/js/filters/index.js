55c5182d4f1a80b1dbe7045159c00e3b1202db10
Grashoper <grashoper20@yahoo.ca>
2017-10-14 19:01:58 -0500

    Filter for formating episode numbers

--- resources/assets/js/filters/index.js
+++ resources/assets/js/filters/index.js
@@ -1,5 +1,8 @@
-
 import moment from 'moment';
+
+function toNormalizedDigits(num, size) {
+    return ('0000' + num).slice(-size);
+}
 
 export default {
     // https://stackoverflow.com/a/18650828.
@@ -12,9 +15,15 @@ export default {
         return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     },
     formatAirDate(date) {
+        if (date === '1') {
+            return 'Never';
+        }
         return moment(date).format('YYYY-MM-DD h:mm:ss a');
     },
-    formatSeasonEpisode(episode) {
-        return 'S' + episode.season + 'E' + episode.episode;
+    formatSeasonEpisode(episode, season_size, episode_size) {
+        season_size = season_size || Math.max(2, episode.season.toString().length);
+        episode_size = episode_size || Math.max(2, episode.episode.toString().length);
+        return 'S' + toNormalizedDigits(episode.season, season_size)
+            + 'E' + toNormalizedDigits(episode.episode, episode_size);
     }
 }
